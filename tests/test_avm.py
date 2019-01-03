b4c55c3260febbfc0ec407dc3d5215b54f51bdfe
pev <pev@sevanssp.com>
2019-01-03 15:17:41 +0100

    Change the way unit tests gets the executable paths to avoid FileNotExist errors.

--- tests/test_avm.py
+++ tests/test_avm.py
@@ -1,21 +1,24 @@
 import unittest
 import os
-from avm import registered_applications, exe_path
-
-
-# path to application listing file
-xml = os.path.join(os.path.dirname(__file__), 'data', 'ApplicationVersions.xml')
+from avm import registered_applications
 
 
 class FirstTestCase(unittest.TestCase):
+    def setUp(self):
+        # path to application listing file
+        self.xml = os.path.join(os.path.dirname(__file__), 'data', 'ApplicationVersions.xml')
+        self.apps = registered_applications(appverxml=self.xml)
+
     def test_number_of_registered_applications(self):
-        self.assertEqual(12, len(registered_applications(appverxml=xml)))
+        self.assertEqual(12, len(self.apps))
 
     def test_sima_path(self):
-        self.assertEqual("C:\programs\sima_35_00\Sima.exe", exe_path("sima", "3.5.0", appverxml=xml))
+        # Cannot use exe_path() which will trow an error since the exe-paths in the xml-file refer to typical
+        # installation directory on laptops.
+        self.assertEqual("C:\programs\sima_35_00\Sima.exe", self.apps['sima']['3.5.0']['exepath'])
 
     def test_prefem_path(self):
-        self.assertEqual("C:\programs\prefem_71_07\\bin\prefem.EXE", exe_path("prefem", "7.1.7", appverxml=xml))
+        self.assertEqual("C:\programs\prefem_71_07\\bin\prefem.EXE", self.apps['prefem']['7.1.7']['exepath'])
 
 
 if __name__ == '__main__':
